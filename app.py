e814fe0803469d90eb37512588360a3444da4faa
zahid2426 <zahidzubair03@gmail.com>
2025-04-27 18:09:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == '__main__':
+    app.run(debug=True)
+
